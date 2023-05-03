83e86a23e3f6fb1f8637107ed7af4f7f53cc95e9
Charles Swaleh <charlswall@live.com>
2023-05-03 14:12:54 +0300

    Created the Session methods for login and logout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,15 @@ class UsersController < ApplicationController
     end
   end
 
+  def show
+    user = User.find_by(id: session[:user_id])
+    if user
+      render json: user, status: :ok
+    else
+      render json: { error: "Not authorized" }, status: :unauthorized
+    end
+  end
+
   private
 
   def user_params
