965b827e7406526c3fbdc901ca759edd595a77e5
Charles Swaleh <charlswall@live.com>
2023-05-03 13:59:20 +0300

    Created the signup route for the user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,22 @@
 class UsersController < ApplicationController
+  wrap_parameters format: []
+
+  def signup
+    user = User.create(user_params)
+    if user.valid?
+      session[:user_id] = user.id
+      render json: user, status: :created
+    else
+      render json: {
+               errors: user.errors.full_messages
+             },
+             status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:username, :password, :password_confirmation)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  resources :users
+  get "/me", to: "users#me"
+  post "/signup", to: "users#signup"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
